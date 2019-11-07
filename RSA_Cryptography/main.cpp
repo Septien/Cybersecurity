d11f2cb7c8ca3ac470da0a7dd67af644bca449a3
Jose Septien <jaseptienh@gmail.com>
2019-11-07 15:46:02 -0600

    Change output format.

--- RSA_Cryptography/main.cpp
+++ RSA_Cryptography/main.cpp
@@ -19,38 +19,38 @@ int main()
     vector<unsigned long int> sK = rsa.getSecretKey();
 
     //
-    cout << "Para los numeros primos: " << p << " " << q << ".\n";
-    cout << "La llave publica es: " << pK[0] << ", " << pK[1] << endl;
-    cout << "La llave privada es: " << sK[0] << ", " << sK[1] << endl;
+    cout << "Para los numeros primos:\n\t " << p << ", " << q << "\n";
+    cout << "La llave publica es:\n\t " << pK[0] << ", " << pK[1] << endl;
+    cout << "La llave privada es:\n\t " << sK[0] << ", " << sK[1] << endl;
 
     // Generar mensajes
     vector<unsigned long int> m;
-    int n = 5;
+    int n = 1;
     for (int i = 0; i < n; i++)
         m.push_back(rand()%10000000);
 
     // Despliega
-    cout << "Mensaje a encriptar: ";
+    cout << "Mensaje a encriptar: \n\t";
     for (int i = 0; i < n; i++)
-        cout << m[i] << ", ";
+        cout << m[i] << " ";
 
     // Encripta y despliega
-    cout << "\nMensaje encriptado: ";
+    cout << "\nMensaje encriptado: \n\t";
     vector<unsigned long int> c;
     for (unsigned int i = 0; i < m.size(); i++)
     {
         c.push_back(rsa.encrypt(m[i]));
-        cout << c.back() << ", ";
+        cout << c.back() << " ";
     }
 
     // Desencripta y despliega
-    cout << "\nMensaje desencriptado: ";
+    cout << "\nMensaje desencriptado: \n\t";
     vector<unsigned long int> mp;
     for (unsigned int i = 0; i < c.size(); i++)
     {
         unsigned long int cm = rsa.decrypt(c[i]);
         mp.push_back(cm);
-        cout << mp.back() << ", ";
+        cout << mp.back() << " ";
     }
     cout << endl;
     return 0;
